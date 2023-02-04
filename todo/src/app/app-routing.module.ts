b6763a430cbb22eaacaa397cf52cb0370e2cf98a
Bruno Costa e Silva <bruno.silva@squadra.com.br>
2023-02-04 18:16:13 -0300

    criando componente error, e configurando rotas

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: '**', redirectTo: 'error' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
